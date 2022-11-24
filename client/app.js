d3e598f0bbfe7e51de08e5222e1f078c91c41559
Grzegorz Woźniak <grzegorz.wozniak1989@gmail.com>
2022-11-24 19:06:02 +0100

    Add chatbot

--- client/app.js
+++ client/app.js
@@ -1,10 +1,41 @@
+const socket = io();
+
+// Listeners
+socket.on('message', ({ author, content }) => addMessage(author, content));
+socket.on('login', ({ user }) => ({ user }));
+socket.on('logAlert', ({ user, status }) => addChatbotMessage(user, status));
+
+// References
 const loginForm = document.getElementById('welcome-form');
 const messagesSection = document.getElementById('messages-section');
 const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
+
 let userName = '';
+
+function addChatbotMessage(user, status) {
+  const message = document.createElement('li');
+  message.classList.add('message', 'message--received');
+  if(status === 'connected'){
+    message.innerHTML = `
+      <h3 class="message__author">ChatBot</h3>
+      <div class="message__content">
+        <i>${user} has joined the conversation!</i>
+      </div>
+      `;
+    messagesList.appendChild(message);
+  } else if(status === 'disconnected'){
+    message.innerHTML = `
+      <h3 class="message__author">ChatBot</h3>
+      <div class="message__content">
+        <i>${user} has left the conversation!</i>
+      </div>
+      `;
+    messagesList.appendChild(message);
+  }
+};
 
 function addMessage(author, content) {
   const message = document.createElement('li');
@@ -23,6 +54,7 @@ loginForm.addEventListener('submit', function login(e) {
   e.preventDefault();
   if(userNameInput.value){
     userName = userNameInput.value;
+    socket.emit('login', (userName))
     loginForm.classList.remove('show');
     messagesSection.classList.add('show');
   } else (
@@ -34,6 +66,8 @@ addMessageForm.addEventListener('submit', function sendMessage(e) {
   e.preventDefault();
   if(messageContentInput.value){
     addMessage(userName, messageContentInput.value);
+    socket.emit('message', ({ author: userName, content: messageContentInput.value }));
+    messageContentInput.value = '';
   } else {
     alert('You must insert your message first');
   };
--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
+
 app = express();
 app.use(express.static(path.join(__dirname, '/client')));
 
 const messages = [];
+const users = [];
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './client/index.html'));
 });
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
   console.log('Server is working on port: 8000');
 });
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('New client! Its id – ' + socket.id);
+    socket.on('message', (message) => { 
+      console.log('Oh, I\'ve got something from ' + socket.id) 
+      messages.push(message);
+      socket.broadcast.emit('message', message);
+    });
+    
+    socket.on('login', (user) => {
+      users.push({ user, id: socket.id });
+      socket.broadcast.emit('logAlert', { user, status: 'connected' });
+      console.log('active users', user);
+    });
+
+    socket.on('disconnect', () => { 
+      const userIndex = users.findIndex((user) => user.id === socket.id);
+      let userName = '' 
+      users.map(conUser => {if(conUser.id === socket.id){ userName = conUser.user}});
+      users.splice(userIndex, 1);
+      socket.broadcast.emit('logAlert', { user: userName , status: 'disconnected' });
+      console.log('Oh, socket ' + socket.id + ' has left') });
+      console.log('I\'ve added a listener on message and disconnect events \n');
+  });
